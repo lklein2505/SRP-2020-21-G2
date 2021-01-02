8144024d685db3d2aa55ee28627fd558957b50bc
lklein2505 <lklein00@fesb.hr>
2021-01-02 17:34:38 +0100

    Implement online dictionary attack

--- postgres-express-node/loaders/express.js
+++ postgres-express-node/loaders/express.js
@@ -14,11 +14,11 @@ module.exports = ({ app, HttpLogger: logger }) => {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.use(jwt({
+  /*app.use(jwt({
     secret: config.jwt.secret,
     algorithms: config.jwt.algorithms,
   }).unless(config.jwt.exclude)
-  );
+  );*/
 
   //---------------------------
   // LOAD/MOUNT API ROUTES
